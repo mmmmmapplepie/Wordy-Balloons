d2b7009a7a6dd487686368312922f9769fbb03bb
mmmmmapplepie <nivedank@gmail.com>
2024-07-11 15:29:28 +0900

    found the lost arts of getting keyboard string

--- Assets/Scripts/inputTeset.cs
+++ Assets/Scripts/inputTeset.cs
@@ -1,10 +1,23 @@
-using System.Collections.Generic;
 using UnityEngine;
-
+using System.Collections;
 public class inputTeset : MonoBehaviour {
 	void Update() {
-		if (Input.anyKey) {
-			// print(Input.ke);
+		string input = Input.inputString;
+		if (input != null && input.Length > 0) {
+			//check enter
+			//if (input == "\r");
+			//check backspace
+			//if (input == "\b");
+
+			//process rest of it.
+			print(input);
 		}
+
 	}
+	// void OnGUI() {
+	// 	Event e = Event.current;
+	// 	if (e.isKey && e.type == EventType.KeyUp) {
+	// 		print(e.character);
+	// 	}
+	// }
 }
